98f17199855f4048366a1e4da60c6359577ce80a
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-10-14 11:22:02 +0200

    change parameter check, and declare helper functions only once, not in the every call

--- R/SignatureFinder.R
+++ R/SignatureFinder.R
@@ -26,31 +26,66 @@ signature.algorithm <- function(table, x, k=100,m=20,p=0.05,
                                 signature.method="p.all.med",
                                 out="signature"){
 
-   if(!is.data.frame(table)){
-      stop(paste("Your data is not a data.frame"))
+   stopifnot(is.data.frame(table))
+
+   stopifnot(x %in% colnames(table))
+
+   stopifnot(k <= ncol(table))
+
+   stopifnot(p<=1, p>=0)
+
+   stopifnot(signature.method %in% c("all.all.med",
+                                     "all.min.max",
+                                     "all.5.med",
+                                     "m.all.max",
+                                     "p.all.med"))
+   stopifnot(out %in% c("signature",
+                        "predictive.SVM",
+                        "predictive.RandomForest"))
+
+   data <- convert.data(table,x)
+   matrix_gene_function <-matrix.gene.GOfunction(data)
+   KW_data <- KW.ranking(data)
+   charakteristic_function <- enriched.GO(data,KW_data,k)
+   if(signature.method=="all.all.med"){
+      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
+      signature <- all.all.med(data_all,KW_data,k)
+   }else if(signature.method=="all.min.max"){
+      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
+      signature <- all.min.max(data_all,KW_data,k)
+   }else if(signature.method=="all.5.med"){
+      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
+      signature <- all.5.med(data_all,KW_data,k)
+   } else if(signature.method=="m.all.max"){
+      data_m <- merge.par.m(KW_data,matrix_gene_function,charakteristic_function,m)
+      signature <- m.all.max(data_m,KW_data,k)
+   } else if(signature.method=="p.all.med"){
+      data_p <- merge.par.p(KW_data,matrix_gene_function,charakteristic_function,p)
+      signature <- p.all.med(data_p,KW_data,k)
+   } else {
+      print("You choose wrong method to find signature: all.all.med,all.min.max,all.5.med,m.all.max,p.all.med are correct value")
    }
-   if(length(which(colnames(table)==x)) == 0 ){
-      stop(paste("There is no column called",x))
+
+
+
+   if(out=="signature"){
+      output <- signature
+   } else if(out=="predictive.SVM"){
+      predictive_method <- SVM.method(data,signature)
+      output <- predictive_method
+   } else if(out=="predictive.RandomForest"){
+      predictive_method <- random.forest.method(data,signature)
+      output <- predictive_method
+   } else {
+      print("You choose wrong output: signature, predictive.SVM, predictive.RandomForest are only correct value ")
    }
-   if(k>dim(table)[2]){
-      stop(paste("Your k is to big"))
-   }
-   if(p>1 |p<0){
-      stop(paste("p mast be a value in [0,1]"))
-   }
-   if(!(signature.method == "all.all.med" |
-        signature.method == "all.min.max" |
-        signature.method == "all.5.med" |
-        signature.method == "m.all.max" |
-        signature.method == "p.all.med" )){
-      stop(paste("There is no signature method called",signature.method))
-   }
-   if(!(out == "signature" |
-        out == "predictive.SVM" |
-        out == "predictive.RandomForest" )){
-      stop(paste("There is no output called",out))
-   }
-   convert.data <- function(table, x){
+
+   return(output)
+
+}
+
+
+      convert.data <- function(table, x){
       index_of_target_wariable <- which(colnames(table)==x)
       table <- cbind(table[,-index_of_target_wariable],table[,index_of_target_wariable])
       colnames(table)[dim(table)[2]] <- "target"
@@ -296,47 +331,3 @@ signature.algorithm <- function(table, x, k=100,m=20,p=0.05,
       return(list(RF_method, signature))
 
    }
-
-
-
-
-   data <- convert.data(table,x)
-   matrix_gene_function <-matrix.gene.GOfunction(data)
-   KW_data <- KW.ranking(data)
-   charakteristic_function <- enriched.GO(data,KW_data,k)
-   if(signature.method=="all.all.med"){
-      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
-      signature <- all.all.med(data_all,KW_data,k)
-   }else if(signature.method=="all.min.max"){
-      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
-      signature <- all.min.max(data_all,KW_data,k)
-   }else if(signature.method=="all.5.med"){
-      data_all <- merge.all.data(KW_data,matrix_gene_function,charakteristic_function)
-      signature <- all.5.med(data_all,KW_data,k)
-   } else if(signature.method=="m.all.max"){
-      data_m <- merge.par.m(KW_data,matrix_gene_function,charakteristic_function,m)
-      signature <- m.all.max(data_m,KW_data,k)
-   } else if(signature.method=="p.all.med"){
-      data_p <- merge.par.p(KW_data,matrix_gene_function,charakteristic_function,p)
-      signature <- p.all.med(data_p,KW_data,k)
-   } else {
-      print("You choose wrong method to find signature: all.all.med,all.min.max,all.5.med,m.all.max,p.all.med are correct value")
-   }
-
-
-
-   if(out=="signature"){
-      output <- signature
-   } else if(out=="predictive.SVM"){
-      predictive_method <- SVM.method(data,signature)
-      output <- predictive_method
-   } else if(out=="predictive.RandomForest"){
-      predictive_method <- random.forest.method(data,signature)
-      output <- predictive_method
-   } else {
-      print("You choose wrong output: signature, predictive.SVM, predictive.RandomForest are only correct value ")
-   }
-
-   return(output)
-
-}
